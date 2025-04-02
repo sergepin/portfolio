ab2279b448d1c900ea8e09f257e4fe71bc00b461
Sergio Pinzon <sergiopin596@gmail.com>
2025-04-01 20:31:32 -0500

    dark mode toggle working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,23 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--text-primary)',
+        },
+        secondary: {
+          DEFAULT: 'var(--text-secondary)',
+        },
+        background: {
+          primary: 'var(--bg-primary)',
+          secondary: 'var(--bg-secondary)',
+        },
+        border: {
+          DEFAULT: 'var(--border-color)',
+        }
+      },
+    },
   },
   plugins: [],
 } 
